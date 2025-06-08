512e7c9118251179e2389507854a807fbef3678e
Sebastian484 <sebastianazucenacc@gmail.com>
2025-06-07 21:30:01 -0500

    primer commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
       <div className="flex justify-center items-center h-full">
         <h1>Hola</h1>
         <p>mundos</p>
-        <p>HOLAAAAAAAA</p>
+        <p>HOLAAAAA</p>
       </div>
     </div>
   );
