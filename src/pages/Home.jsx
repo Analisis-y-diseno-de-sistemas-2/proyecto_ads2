7dca0dc62148790e2107147e877c1d2af730a68c
Ricardo <ricardo987827627@gmail.com>
2025-06-13 00:13:45 -0500

    mi primer commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
       <div className="flex justify-center items-center h-full">
         <h1>Hola</h1>
         <p>mundos</p>
+        <a href="">Nuevo proyecto</a>
       </div>
     </div>
   );
