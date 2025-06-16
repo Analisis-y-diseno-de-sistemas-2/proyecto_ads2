641ffdb354dd888c234070b2f382187add94a652
Maki Giuseppe de la Vega Silva <pankthera@gmail.com>
2025-06-16 00:30:17 -0500

    refactor: remove commented-out button logic in ButtonControl component

--- src/components/stepercontrol/ButtonControl.jsx
+++ src/components/stepercontrol/ButtonControl.jsx
@@ -23,6 +23,16 @@ const ButtonControl = ({ handleClick, currentStep, steps }) => {
       >
         {currentStep === steps.length - 1 ? "Confirmar" : "Continuar"}
       </button>
+
+      {/* {currentStep < 3 && (
+        <button
+          onClick={() => handleClick("next")}
+          className="bg-[#92C137] text-white uppercase w-32 py-2 px-4 rounded-xl font-semibold cursor-pointer  hover:bg-slate-700 hover:text-white
+        transition duration-200 ease-in-out"
+        >
+          {currentStep === steps.length - 1 ? "Confirmar" : "Continuar"}
+        </button>
+      )} */}
     </div>
   );
 };
