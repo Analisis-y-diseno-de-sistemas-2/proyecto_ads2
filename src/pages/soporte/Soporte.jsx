426df8760e0873b3eee5364933fda3114c636b95
Sebastian484 <sebastianazucenacc@gmail.com>
2025-06-19 12:30:33 -0500

    resuelto

--- src/pages/soporte/Soporte.jsx
+++ src/pages/soporte/Soporte.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Soporte() {
-  return <div>Soporte</div>;
+  return <div>
+
+    <div className="w-full">
+      <img src="../public/bannersoporte.png" alt="bannersoporte" className="w-full block"/>
+    </div>
+    
+  </div>;
 }
 
 export default Soporte;
